6559830d640b81383cabbc666288baa26969ed9f
nikita <nikitacohen7@gmail.com>
2022-05-29 11:08:31 +0300

    change worker

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-const {Worker, workerData} = require("worker_threads");
+const {Worker} = require("worker_threads");
 
 const data = [
     {url : "https://archive.org/details/manuals?&sort=titleSorter&page=1"},
@@ -6,22 +6,27 @@ const data = [
     {url : "https://archive.org/details/manuals?&sort=titleSorter&page=3"}
 ]
 
-async function runWorker() {
-    const result = await Promise.all(data.map((obj, index) => {
-        new Promise((resolve, reject) =>  {
-            const worker = new Worker('./workerThread', {
-                workerData : obj
-            })
-
-            worker.on("message", resolve);
-            worker.on("error", reject);
-            worker.on("exit", (code) => {
-                if (code !== 0) reject(new Error("something go wrong"));
-            })
+async function runWorker(obj) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./workerThread', {
+            workerData : obj
         })
-    }))
 
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+            if (code !== 0) reject(new Error("something go wrong"));
+        })
+    })
 }
+
+data.forEach(obj => {
+    runWorker(obj).then(data => {
+        if (data) {
+            console.log(data)
+        }
+    })
+})
 
 function resetAtMidnight() {
     let now = new Date();
@@ -35,11 +40,12 @@ function resetAtMidnight() {
     let msToMidnight = night.getTime() - now.getTime();
 
     setTimeout(function() {
-        runWorker().then();            //      <-- This is the function being called at midnight.
+        runWorker().then();           //      <-- This is the function being called at midnight.
         resetAtMidnight();    //      Then, reset again next midnight.
     }, msToMidnight);
 }
 
+runWorker().then();
 resetAtMidnight();
 
 
