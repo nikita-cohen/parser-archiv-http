b6fcc1716fc8ad277aba9c0728c81ff459321518
nikita <nikitacohen7@gmail.com>
2022-06-01 15:06:37 +0300

    change worker

--- Main.js
+++ Main.js
@@ -34,7 +34,7 @@ function resetAtMidnight() {
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + 1, // the next day, ...
-        0, 0, 0 // ...at 00:00:00 hours
+        5, 0, 0 // ...at 00:00:00 hours
     );
 
     let msToMidnight = night.getTime() - now.getTime();
